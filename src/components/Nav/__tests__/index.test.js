05a5c0e7a534791f9af645a629806d27f996316d
Bryant <brt9029@gmail.com>
2022-02-15 03:28:55 -0600

    added tests for Nav

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import About from '..';
+import Nav from '..';
 
 afterEach(cleanup);
 
-describe('About component', () => {
+describe('Nav component', () => {
     it('renders', () => {
-        render(<About />);
+        render(<Nav />)
     });
 
-    it('matches snapshot DOM node structure', () => {
-        const { asFragment } = render(<About />);
+    it('matches snapshot', () => {
+        const { asFragment } = render(<Nav />);
 
-        expect(asFragment()).toMatchSnapshot();
+        expect(asFragment).toMatchSnapshot();
     });
-})
+});
+
+describe('emoji is visible', () => {
+    it('inserts emoji into the h2', () => {
+        const { getByLabelText } = render(<Nav />);
+
+        expect(getByLabelText('camera')).toHaveTextContent('📸');
+    });
+});
+
+describe('links are visible', () => {
+    it('inserts text into the links', () => {
+        const { getByTestId } = render(<Nav />);
+
+        expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+        expect(getByTestId('about')).toHaveTextContent('About me');
+    });
+});
