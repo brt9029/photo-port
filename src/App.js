665b41fd29b4c89b7fabeb1ffb7bd3069bd8be8a
Bryant <brt9029@gmail.com>
2022-02-15 05:49:34 -0600

    photo gallery created

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { useState } from 'react';
-import About from './components/About';
 import Nav from './components/Nav';
+import About from './components/About';
 import Gallery from './components/Gallery';
 
 function App() {
-   
-const [categories] = useState([
-  { name: "commercial", description: "Photos of grocery stores, food trucks, and other commercial projects"},
-  { name: "portraits", description: "Portraits of people in my life" },
-  { name: "food", description: "Delicious delicacies" },
-  { name: "landscape", description: "Fields, farmhouses, waterfalls, and the beauty of nature"},
+  const [categories] = useState([
+    {
+      name: 'commercial',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+    },
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
   ]);
 
-const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
   return (
     <div>
@@ -20,10 +22,12 @@ const [currentCategory, setCurrentCategory] = useState(categories[0]);
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
-        ></Nav>
+      ></Nav>
       <main>
-        <Gallery></Gallery>
-        <About></About>
+        <div>
+          <Gallery currentCategory={currentCategory}></Gallery>
+          <About></About>
+        </div>
       </main>
     </div>
   );
