139817b885dad3c323948dcb8f2cadca49380f62
Bryant <brt9029@gmail.com>
2022-02-17 04:45:28 -0600

    added SPA function to render items as needed when selected from Nav bar

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,17 +1,35 @@
 import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
 
     const { name, email, message } = formState;
 
+    const [errorMessage, setErrorMessage] = useState('');
+
     function handleChange(e) {
-        setFormState({...formState, [e.target.name]: e.target.value})
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            if(!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if(!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+        if(!errorMessage) {
+            setFormState({...formState, [e.target.name]: e.target.value})
+        }
     }
 
     function handleSubmit(e) {
         e.preventDefault();
-        console.log(formState);
     }
 
     return (
@@ -20,16 +38,21 @@ function ContactForm() {
             <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" defaultValue={name} name="name" onChange={handleChange} />
+                    <input type="text" defaultValue={name} name="name" onBlur={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" defaultValue={email} name="email" onChange={handleChange} />
+                    <input type="email" defaultValue={email} name="email" onBlur={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" defaultValue={message} row="5" onChange={handleChange} />
+                    <textarea name="message" defaultValue={message} row="5" onBlur={handleChange} />
                 </div>
+                {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
                 <button type="submit">Submit</button>
             </form>
         </section>
