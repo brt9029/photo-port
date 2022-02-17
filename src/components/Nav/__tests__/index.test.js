38bd78153c725fb15a2a7c8135e1ff976b7272af
Bryant <brt9029@gmail.com>
2022-02-17 06:08:15 -0600

    added and confirmed passing tests for contact form rendering

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -3,12 +3,25 @@ import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
+const categories = [
+  { name: 'portraits', description: 'Portraits of people in my life' }
+]
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
 
 afterEach(cleanup);
 
 describe('Nav component', () => {
   it('renders', () => {
-    render(<Nav />);
+    render(<Nav
+      categories={categories}
+      setCurrentCategory={mockSetCurrentCategory}
+      currentCategory={mockCurrentCategory}
+      contactSelected={mockContactSelected}
+      setContactSelected={mockSetContactSelected}
+    />);
   });
 
   it('matches snapshot', () => {
@@ -16,7 +29,7 @@ describe('Nav component', () => {
 
     expect(asFragment()).toMatchSnapshot();
   });
-})
+});
 
 describe('emoji is visible', () => {
   it('inserts emoji into the h2', () => {
@@ -24,7 +37,7 @@ describe('emoji is visible', () => {
 
     expect(getByLabelText('camera')).toHaveTextContent('📸');
   });
-})
+});
 
 describe('links are visible', () => {
   it('inserts text into the links', () => {
@@ -34,4 +47,4 @@ describe('links are visible', () => {
     expect(getByTestId('about')).toHaveTextContent('About me');
   });
 
-})
+});
